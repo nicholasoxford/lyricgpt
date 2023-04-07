9b44528315c9efe73edc3c3244591ed8f3d46ddf
Nicholas Oxford <maheledaily@gmail.com>
2023-04-07 07:13:10 -0600

    remove mdx dependecy

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    mdxRs: true,
   },
 };
 
-const withMDX = require("@next/mdx")();
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
